f1a93b0733878aa46be64302d3a389dea7994db1
sander <sander.philipsen@hotmail.com>
2018-10-26 11:09:47 +0200

    registreer

--- src/main/java/be/thomasmore/travelmore/controller/GebruikerController.java
+++ src/main/java/be/thomasmore/travelmore/controller/GebruikerController.java
@@ -4,25 +4,28 @@ import be.thomasmore.travelmore.domain.Gebruiker;
 import be.thomasmore.travelmore.service.GebruikerService;
 import be.thomasmore.travelmore.domain.Gebruiker;
 
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 @ManagedBean
 @ViewScoped
+
 public class GebruikerController {
     private Gebruiker nieuweGebruiker = new Gebruiker();
 
     @Inject
-    GebruikerService gebruikerService;
+   private GebruikerService gebruikerService;
 
-    public Gebruiker getNieuweGebruiker() {
-        return nieuweGebruiker;
+    public Gebruiker getNieuweGebruiker() { return nieuweGebruiker;
     }
 
     public void setNieuweGebruiker(Gebruiker nieuweGebruiker) {
         this.nieuweGebruiker = nieuweGebruiker;
     }
 
+    public void registreer(){
 
+
+    }
 }
