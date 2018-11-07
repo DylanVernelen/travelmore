578113343bfe86f2c749ed054322927fc8be0f29
Senne <sennecools@hotmail.com>
2018-11-07 11:31:23 +0100

    Start loginfunctionaliteit

--- src/main/java/be/thomasmore/travelmore/controller/GebruikerController.java
+++ src/main/java/be/thomasmore/travelmore/controller/GebruikerController.java
@@ -5,16 +5,22 @@ import be.thomasmore.travelmore.service.GebruikerService;
 import be.thomasmore.travelmore.domain.Gebruiker;
 
 import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class GebruikerController {
     private Gebruiker nieuweGebruiker = new Gebruiker();
+    private String gebruikernaam;
+    private String wachtwoord;
 
     @Inject
     GebruikerService gebruikerService;
+
+    
 
     public Gebruiker getNieuweGebruiker() {
         return nieuweGebruiker;
@@ -22,5 +28,21 @@ public class GebruikerController {
 
     public void setNieuweGebruiker(Gebruiker nieuweGebruiker) {
         this.nieuweGebruiker = nieuweGebruiker;
+    }
+
+    public String getGebruikernaam() {
+        return gebruikernaam;
+    }
+
+    public void setGebruikernaam(String gebruikernaam) {
+        this.gebruikernaam = gebruikernaam;
+    }
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
     }
 }
