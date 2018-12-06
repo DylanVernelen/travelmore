3420fdfcf1624321a9c3d937fab11154dc3c0953
sander <sander.philipsen@hotmail.com>
2018-12-06 15:14:54 +0100

    registeren validation

--- src/main/resources/sql/travel_more_ddl.sql
+++ src/main/resources/sql/travel_more_ddl.sql
@@ -138,6 +138,9 @@ INSERT INTO `transportmiddel` (`id`, `naam`) VALUES
 	(2, 'Vliegtuig'),
 	(3, 'Bus');
 
+INSERT INTO `rol` (`id`, `naam`) VALUES
+	(1, 'Gebruiker'),
+	(2, 'Admin');
 INSERT INTO `reis` (`id`, `naam`, `afbeelding`, `vertrek_id`, `bestemming_id`, `aantalPlaatsen`, `kostprijs`, `startdatum`, `einddatum`, `transportmiddel_id`) VALUES
 	(2, 'Reis naar Parijs', 'parijs.jpg', 2, 2, 10, 249, '2018-01-01 00:00:00', '2018-01-07 00:00:00', 1),
 	(3, 'Driedaagse naar Berlijn', 'berlijn.jpg', 3, 4, 25, 399, '2018-01-08 00:00:00', '2018-01-11 00:00:00', 2),
